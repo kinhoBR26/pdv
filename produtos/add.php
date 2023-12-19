132c339fa0bd13dbee6a5bcea8c3ecf7e51a3587
marcus <marcusvastraub@gmail.com>
2023-12-19 19:09:29 -0300

    campo de img no add produtos.

--- produtos/add.php
+++ produtos/add.php
@@ -2,19 +2,21 @@
 session_start();
 include('../seguranca.php');
 include('../conexao.php');
-if($_POST){
+if($_FILES){
     $usuario_id = $_SESSION['id'];
     $nome = $_POST['nome'];
     $preco = $_POST['preco'];
     $quantidade = $_POST['quantidade'];
-    $sql = "insert into produtos (nome,preco,quantidade,usuario_id) values ('$nome','$preco','$quantidade','$usuario_id')";
+    $dir = "../img_produtos";
+    $file = $_FILES['img'];
+    $caminho_da_img = $dir."/".$file['name'];
+    move_uploaded_file($file["tmp_name"], $caminho_da_img);
+    $sql = "insert into produtos (nome,preco,quantidade,img,usuario_id) values ('$nome','$preco','$quantidade','$caminho_da_img','$usuario_id')";
     $stmt = $conexao->prepare($sql);
     $stmt->execute();
     header("location: index.php");
 }
 ?>
-
-
 <html>
 <head>
 <title>Cadastro de produtos</title>
@@ -26,7 +28,7 @@ if($_POST){
     <?php include '../menu.php';?>
 <h1>Cadastro de produtos</h1>
 
-<form method="post" action="add.php">
+<form method="post" action="add.php" enctype="multipart/form-data">
 
     <div class="mb-3">
         <label class="form-label">Nome Do Produto:</label>
@@ -42,6 +44,12 @@ if($_POST){
         <label class="form-label">Quantidade produto:</label>
         <input type="number" name="quantidade" placeholder="Digite a quantidade do produto" required class="form-control">
     </div>
+
+    <div class="mb-3">
+        <label class="form-label">Imagem produto:</label>
+        <input type="file" name="img" required class="form-control">
+    </div>
+
     <button type="submit" class="btn btn-primary">Cadastrar produto</button>
 
 </form>
