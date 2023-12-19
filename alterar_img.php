f3e8886f43a4c9e5a3269bc3710c2a923b0642ce
kinhoBR26 <111158410+kinhoBR26@users.noreply.github.com>
2023-12-19 17:31:42 -0300

    Update alterar_img.php
    texto botão alterar imagem.

--- alterar_img.php
+++ alterar_img.php
@@ -31,7 +31,7 @@ if($_FILES){
     <?php include 'menu.php';?>
     <form method="post" action="alterar_img.php" enctype="multipart/form-data">
         <input type="file" name="img" class="form-control">
-        <button type="submit" class="btn btn-primary">Atualizar Senha</button>
+        <button type="submit" class="btn btn-primary">Atualizar imagem</button>
     </form>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
