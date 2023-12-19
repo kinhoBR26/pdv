6322d1627ccaf00b703f7f67b00d2f6352c49111
marcus <marcusvastraub@gmail.com>
2023-12-19 18:10:21 -0300

    Revert "mos"
    This reverts commit 7945d080c2a686eb13dcdfce60150c2452af2956.

--- produtos/index.php
+++ produtos/index.php
@@ -5,7 +5,9 @@ include('../conexao.php');
 <html>
 <head>
     <title>Listar Produtos</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
 <body>
 <div class="container">
@@ -19,7 +21,6 @@ include('../conexao.php');
             <th scope="col">Nome</th>
             <th scope="col">Preco</th>
             <th scope="col">Quantidade</th>
-            <th scope="col">Imagem</th>
         </tr>
         </thead>
         <tbody>
@@ -32,7 +33,6 @@ include('../conexao.php');
                 <td><?php echo $linha["nome"]?></td>
                 <td><?php echo $linha["preco"]?></td>
                 <td><?php echo $linha["quantidade"]?></td>
-                <td><a href="<?php echo $linha["img"]?>" target="_blank"><img src="<?php echo $linha["img"]?>" width="40px"height="auto"></a></td>
                 <td>
                     <a href="delete.php?id=<?php echo $linha["id"]?>" onclick="return confirm('Deseja realmente excluir?')"><button type="button" class="btn btn-danger">Excluir</button></a>
                     <a href="update.php?id=<?php echo $linha["id"]?>"> <button type="button" class="btn btn-dark">Editar</button></a>
